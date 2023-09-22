2b6b5b0ca701042a0f5333d2692cd0ec94f891f6
Mikhail Odebe <derpiranha@gmail.com>
2023-09-23 03:46:16 +0400

    removing fx test for mysql

--- spec/generators/mysql/install_generator_spec.rb
+++ spec/generators/mysql/install_generator_spec.rb
@@ -27,37 +27,9 @@ describe Logidze::Generators::InstallGenerator, type: :generator, database: :mys
       is_expected.to exist
       is_expected.to contain "ActiveRecord::Migration[#{ar_version}]"
     end
-
-    xcontext "when using fx" do
-      let(:fx_args) { use_fx_args }
-
-      it "creates migration", :aggregate_failures do
-        run_generator(args)
-
-        is_expected.to exist
-        is_expected.to contain "ActiveRecord::Migration[#{ar_version}]"
-      end
-
-      it "creates function files" do
-        run_generator(args)
-
-        is_expected.to exist
-        %w[
-          logidze_logger_v04.sql
-          logidze_logger_after_v04.sql
-          logidze_version_v02.sql
-          logidze_filter_keys_v01.sql
-          logidze_snapshot_v03.sql
-          logidze_compact_history_v01.sql
-          logidze_capture_exception_v01.sql
-        ].each do |path|
-          expect(file("db/functions/#{path}")).to exist
-        end
-      end
-    end
   end
 
-  xcontext "update migration" do
+  context "update migration" do
     let(:version) { Logidze::VERSION.delete(".") }
     let(:base_args) { ["--update"] }
 
@@ -69,58 +41,6 @@ describe Logidze::Generators::InstallGenerator, type: :generator, database: :mys
       expect(migration_file("db/migrate/logidze_install.rb")).not_to exist
 
       is_expected.to exist
-    end
-
-    context "when using fx" do
-      let(:fx_args) { use_fx_args }
-
-      let(:existing) do
-        %w[
-          logidze_version_v03.sql
-          logidze_snapshot_v4.sql
-          logidze_filter_keys_v01.sql
-          logidze_compact_history_v05.sql
-          logidze_logger_v7.sql
-          logidze_logger_after_v7.sql
-        ]
-      end
-
-      before do
-        FileUtils.mkdir_p(file("db/functions"))
-        existing.each do |path|
-          File.write(file("db/functions/#{path}"), "")
-        end
-      end
-
-      after do
-        existing.each do |path|
-          File.delete(file("db/functions/#{path}"))
-        end
-
-        FileUtils.rm_r(file("db/functions"))
-      end
-
-      it "creates migration", :aggregate_failures do
-        run_generator(args)
-
-        is_expected.to exist
-      end
-
-      it "creates function files" do
-        run_generator(args)
-
-        is_expected.to exist
-        %w[
-          logidze_logger_v04.sql
-          logidze_logger_after_v04.sql
-          logidze_version_v02.sql
-          logidze_snapshot_v03.sql
-          logidze_compact_history_v01.sql
-          logidze_capture_exception_v01.sql
-        ].each do |path|
-          expect(file("db/functions/#{path}")).to exist
-        end
-      end
     end
   end
 end
