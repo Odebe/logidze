9ea1e5dfa116b5cd1424f3839fbafdb6e44012f8
Mikhail Odebe <derpiranha@gmail.com>
2023-09-23 05:08:24 +0400

    mysql logidze_compact_history fix

--- lib/generators/logidze/install/mysql/functions/logidze_compact_history.sql
+++ lib/generators/logidze/install/mysql/functions/logidze_compact_history.sql
@@ -1,23 +1,25 @@
 CREATE FUNCTION logidze_compact_history(log_data json, cutoff integer) RETURNS json NO SQL
 BEGIN
     -- version: 1
-    DECLARE cutoff integer default 1;
-    DECLARE new_version json;
-    DECLARE result json;
+    DECLARE new_history_item json;
+
+    IF cutoff IS NULL THEN
+        SET cutoff = 1;
+    END IF;
 
     iteration: LOOP
-      SET new_version = JSON_OBJECT(
+      SET new_history_item = JSON_OBJECT(
           'ts', JSON_EXTRACT(log_data, '$.h[1].ts'),
           'v',  JSON_EXTRACT(log_data, '$.h[1].v'),
           'c',  JSON_MERGE_PATCH(JSON_EXTRACT(log_data, '$.h[0].c'), JSON_EXTRACT(log_data, '$.h[1].c'))
       );
 
       IF JSON_CONTAINS_PATH(log_data, 'one', '$.h[1].m') THEN
-          SET new_version = JSON_INSERT(new_version, '$.m', JSON_EXTRACT(log_data, '$.h[1].m'));
+          SET new_history_item = JSON_INSERT(new_history_item, '$.m', JSON_EXTRACT(log_data, '$.h[1].m'));
       END IF;
 
-      SET new_version = JSON_REPLACE(log_data, '$.h[1]', new_version);
-      SET result = JSON_REMOVE(new_version, '$.h[0]');
+      SET new_history_item = JSON_REPLACE(log_data, '$.h[1]', new_history_item);
+      SET log_data = JSON_REMOVE(new_history_item, '$.h[0]');
 
       SET cutoff = cutoff - 1;
 
@@ -26,5 +28,5 @@ BEGIN
       END IF;
     END LOOP iteration;
 
-    RETURN result;
+    RETURN log_data;
 END;
