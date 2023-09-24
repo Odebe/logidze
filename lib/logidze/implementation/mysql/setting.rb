98250894ccf087e6b0ca56bf86e5b4f2136aa924
Mikhail Odebe <derpiranha@gmail.com>
2023-09-25 01:05:32 +0400

    mysql: clear setting in ruby instead of error handling

--- lib/logidze/implementation/mysql/setting.rb
+++ lib/logidze/implementation/mysql/setting.rb
@@ -7,6 +7,16 @@ module Logidze
     module Mysql
       module Setting
         class Wrapper < Abstract::Setting::Wrapper
+
+          def within_setting
+            db_set_setting_param
+
+            yield
+          ensure
+            # TODO: create exception handler in sql functions
+            db_clear_setting_param
+          end
+
           def db_set_setting_param
             connection.execute "SET @#{name} = '#{value}';"
           end
